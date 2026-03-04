eaf5e776b4f0e77b808a91b53a234744459ba093
cmpaadmin <cmpa-admin@fsu.edu>
2026-03-04 16:04:53 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import csv from "csv-parser";
 import https from "https";
 
 const ADMIN_PASSWORD = "f1!msk00lF$U";
-const GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1mfO73uNkVsp64qdkmgdUsOO93xiyk7gbxxfocIBYd7A/edit?gid=1643430082#gid=1643430082";
+const GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOW0D_7N_4XAuMQKM71quXgdPKFj3h52QF_rCAIo5-Uo3WQAjDOqHQr3JrfiemGkh644Yp-W8G2PrF/pub?output=csv";
 
 const app = express();
 const server = http.createServer(app);
